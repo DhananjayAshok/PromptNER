ac58a74834c534b79dce4b826efb326d5e92d64c
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2023-03-21 00:07:10 +0530

    Updated eval

--- eval.py
+++ eval.py
@@ -16,22 +16,26 @@ def fp(x, pred_col="preds"):
     return list(preds.difference(entities))
 
 
+def lower_map(x):
+    return [i.lower() for i in x]
+
+
 def split(x):
     new_l = []
     for item in x:
         new_l.extend(item.split(" "))
-    return []
+    return new_l
 
 
 def get_results_frame(filename, results_dir="results", split_phrases=False, clean_output=True):
     if ".csv" not in filename:
         filename = filename + ".csv"
     df = pd.read_csv(results_dir+"/"+filename)
-    df["preds"] = df["preds"].apply(eval)
-    df["entities"] = df["entities"].apply(eval)
+    df["preds"] = df["preds"].apply(eval).apply(lower_map)
+    df["entities"] = df["entities"].apply(eval).apply(lower_map)
     df["fn"] = df.apply(fn, axis=1)
     df["fp"] = df.apply(fp, axis=1)
-    df["candidates"] = df.apply(lambda x: AnswerMapping.exemplar_format_list(x["meta"], true_only=False), axis=1)
+    df["candidates"] = df.apply(lambda x: AnswerMapping.exemplar_format_list(x["meta"], true_only=False), axis=1).apply(lower_map)
     if split_phrases:
         df["candidates"] = df["candidates"].apply(split)
     if clean_output:
@@ -40,12 +44,18 @@ def get_results_frame(filename, results_dir="results", split_phrases=False, clea
     df["candidate_fp"] = df.apply(lambda x: fp(x, pred_col="candidates"), axis=1)
     print(f"Aggregate Analysis on {filename}:")
     print(f"On Predictions")
-    print(f"\tFalse Positives (Mean: {df['fp'].mean()} Std: {df['fp'].std()})")
-    print(f"\tFalse Negatives (Mean: {df['fn'].mean()} Std: {df['fn'].std()})")
+    print(f"\tFalse Positives (Mean: {df['fp'].apply(len).mean()} Std: {df['fp'].apply(len).std()})")
+    print(f"\tFalse Negatives (Mean: {df['fn'].apply(len).mean()} Std: {df['fn'].apply(len).std()})")
     print(f"On Candidates")
-    print(f"\tFalse Positives (Mean: {df['candidate_fp'].mean()} Std: {df['candidate_fp'].std()})")
-    print(f"\tFalse Negatives (Mean: {df['candidate_fn'].mean()} Std: {df['candidate_fn'].std()})")
+    print(f"\tFalse Positives (Mean: {df['candidate_fp'].apply(len).mean()} Std: {df['candidate_fp'].apply(len).std()})")
+    print(f"\tFalse Negatives (Mean: {df['candidate_fn'].apply(len).mean()} Std: {df['candidate_fn'].apply(len).std()})")
     return df
 
 
+def bulk_eval():
+    filenames = [ "GPT3_conllNone.csv", "GPT3_crossnerai.csv", "GPT3_crossnerliterature.csv",
+                  "GPT3_crossnermusic.csv", "GPT3_crossnerpolitics.csv", "GPT3_crossnerscience.csv"]
+    for filename in filenames:
+        split_phrases = "conll" in filename
+        get_results_frame(filename, split_phrases=split_phrases)
 
