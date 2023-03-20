6b07dd9d29ee5ebabebcbab815ba35ea079ab7c7
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2023-03-21 00:20:20 +0530

    Generous inclusion eval

--- eval.py
+++ eval.py
@@ -16,6 +16,19 @@ def fp(x, pred_col="preds"):
     return list(preds.difference(entities))
 
 
+def generous_inclusion(x, pred_col="preds"):
+    incls = []
+    for item in x["entities"]:
+        flag = False
+        for pred in x[pred_col]:
+            if item in pred:
+                flag = True
+                break
+        if not flag:
+            incls.append(item)
+    return incls
+
+
 def lower_map(x):
     return [i.lower() for i in x]
 
@@ -35,6 +48,7 @@ def get_results_frame(filename, results_dir="results", split_phrases=False, clea
     df["entities"] = df["entities"].apply(eval).apply(lower_map)
     df["fn"] = df.apply(fn, axis=1)
     df["fp"] = df.apply(fp, axis=1)
+    df["gen_fn"] = df.apply(generous_inclusion, axis=1)
     df["candidates"] = df.apply(lambda x: AnswerMapping.exemplar_format_list(x["meta"], true_only=False), axis=1).apply(lower_map)
     if split_phrases:
         df["candidates"] = df["candidates"].apply(split)
@@ -42,13 +56,17 @@ def get_results_frame(filename, results_dir="results", split_phrases=False, clea
         df["candidates"] = df["candidates"].apply(BaseAlgorithm.clean_output)
     df["candidate_fn"] = df.apply(lambda x: fn(x, pred_col="candidates"), axis=1)
     df["candidate_fp"] = df.apply(lambda x: fp(x, pred_col="candidates"), axis=1)
+    df["candidate_gen_fn"] = df.apply(lambda x: generous_inclusion(x, pred_col="candidates"), axis=1)
     print(f"Aggregate Analysis on {filename}:")
     print(f"On Predictions")
     print(f"\tFalse Positives (Mean: {df['fp'].apply(len).mean()} Std: {df['fp'].apply(len).std()})")
     print(f"\tFalse Negatives (Mean: {df['fn'].apply(len).mean()} Std: {df['fn'].apply(len).std()})")
+    print(f"\tGenerous False Negatives (Mean: {df['gen_fn'].apply(len).mean()} Std: {df['gen_fn'].apply(len).std()})")
+
     print(f"On Candidates")
     print(f"\tFalse Positives (Mean: {df['candidate_fp'].apply(len).mean()} Std: {df['candidate_fp'].apply(len).std()})")
     print(f"\tFalse Negatives (Mean: {df['candidate_fn'].apply(len).mean()} Std: {df['candidate_fn'].apply(len).std()})")
+    print(f"\tGenerous False Negatives (Mean: {df['candidate_gen_fn'].apply(len).mean()} Std: {df['candidate_gen_fn'].apply(len).std()})")
     return df
 
 
