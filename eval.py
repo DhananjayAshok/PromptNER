af26e5e41403af424ab39ba75bbce70cf6b025a1
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2023-06-22 22:08:07 +0530

    Eval Additions

--- eval.py
+++ eval.py
@@ -8,125 +8,48 @@ from data import scroll
 import warnings
 import random
 from seqeval.metrics import f1_score as seq_f1
+results_dir = "results"
 
-def fn(x, pred_col="preds"):
-    preds = set(x[pred_col])
-    entities = set(x["entities"])
-    return list(entities.difference(preds))
-
-
-def fp(x, pred_col="preds"):
-    preds = set(x[pred_col])
-    entities = set(x["entities"])
-    return list(preds.difference(entities))
-
-
-def generous_inclusion(x, pred_col="preds"):
-    incls = []
-    for item in x["entities"]:
-        flag = False
-        for pred in x[pred_col]:
-            if item in pred:
-                flag = True
-                break
-        if not flag:
-            incls.append(item)
-    return incls
-
-
-def lower_map(x):
-    return [i.lower() for i in x]
-
-
-def split(x):
-    new_l = []
-    for item in x:
-        new_l.extend(item.split(" "))
-    return new_l
-
-
-def get_results_frame(filename, results_dir="results", split_phrases=False, clean_output=True):
-    if ".csv" not in filename:
-        filename = filename + ".csv"
-    df = pd.read_csv(results_dir+"/"+filename)
-    df["preds"] = df["preds"].apply(eval).apply(lower_map)
-    df["entities"] = df["entities"].apply(eval).apply(lower_map)
-    df["fn"] = df.apply(fn, axis=1)
-    df["fp"] = df.apply(fp, axis=1)
-    df["gen_fn"] = df.apply(generous_inclusion, axis=1)
-    df["candidates"] = df.apply(lambda x: AnswerMapping.exemplar_format_list(x["meta"], true_only=False), axis=1).apply(lower_map)
-    if split_phrases:
-        df["candidates"] = df["candidates"].apply(split)
-    if clean_output:
-        df["candidates"] = df["candidates"].apply(BaseAlgorithm.clean_output)
-    df["candidate_fn"] = df.apply(lambda x: fn(x, pred_col="candidates"), axis=1)
-    df["candidate_fp"] = df.apply(lambda x: fp(x, pred_col="candidates"), axis=1)
-    df["candidate_gen_fn"] = df.apply(lambda x: generous_inclusion(x, pred_col="candidates"), axis=1)
-    print(f"Aggregate Analysis on {filename}:")
-    print(f"On Predictions")
-    print(f"\tFalse Positives (Mean: {df['fp'].apply(len).mean()} Std: {df['fp'].apply(len).std()})")
-    print(f"\tFalse Negatives (Mean: {df['fn'].apply(len).mean()} Std: {df['fn'].apply(len).std()})")
-    print(f"\tGenerous False Negatives (Mean: {df['gen_fn'].apply(len).mean()} Std: {df['gen_fn'].apply(len).std()})")
-
-    print(f"On Candidates")
-    print(f"\tFalse Positives (Mean: {df['candidate_fp'].apply(len).mean()} Std: {df['candidate_fp'].apply(len).std()})")
-    print(f"\tFalse Negatives (Mean: {df['candidate_fn'].apply(len).mean()} Std: {df['candidate_fn'].apply(len).std()})")
-    print(f"\tGenerous False Negatives (Mean: {df['candidate_gen_fn'].apply(len).mean()} Std: {df['candidate_gen_fn'].apply(len).std()})")
-    return df
-
-
-def bulk_eval(filenames=[ "GPT4QOpenAIGPT_conllNone.csv", "GPT4QOpenAIGPT_crossnerai.csv",
-                          "GPT4QOpenAIGPT_crossnerliterature.csv", "GPT4QOpenAIGPT_crossnermusic.csv",
-                          "GPT4QOpenAIGPT_crossnerpolitics.csv", "GPT4QOpenAIGPT_crossnerscience.csv",
-                          "GPT4QOpenAIGPT_fewnerdtest.csv"],
-              keys=["conll", "ai", "lit", "music", "pol", "science", "fewnerd"]):
-    dfs = {}
-    for i, filename in enumerate(filenames):
-        split_phrases = "conll" in filename
-        df = get_results_frame(filename, split_phrases=split_phrases)
-        dfs[keys[i]] = df
-    return dfs
 
 
 def is_eq(e1, e2):
     return e1.lower().strip().strip(string.punctuation).strip() == e2.lower().strip().strip(string.punctuation).strip()
 
 
-def f1(true_list, pred_list):
-    true_list = list(set(true_list))
-    pred_list = list(set(pred_list))
-    tp = 0
-    fn = 0
-    fp = 0
-    if len(true_list) == 0:
-        if len(pred_list) != 0:
-            return 0, tp, len(pred_list), fn
-        else:
-            return 1, tp, fp, fn
-    if len(pred_list) == 0:
-        if len(true_list) != 0:
-            return 0, tp, fp, len(true_list)
-        else:
-            return 1, tp, fp, fn
-    for positive in true_list:
-        flag = False
-        for candidate in pred_list:
-            if is_eq(positive, candidate):
-                tp += 1
-                flag = True
-                break
-        if not flag:
-            fn += 1
-    for candidate in pred_list:
-        flag = False
-        for positive in true_list:
-            if is_eq(positive, candidate):
-                flag=True
-                break
-        if not flag:
-            fp += 1
-    f1_score = tp/(tp + 0.5*(fp+fn))
-    return f1_score, tp, fp, fn
+def basic_process_results(filename):
+    df = pd.read_csv(results_dir+"/"+filename)
+    for col in ["entities", "truth", "pred"]:
+        df[col] =df[col].apply(eval)
+    df['pred_text'] = None
+    df['truth_text'] = None
+    df['correct'] = df['pred'] == df['truth']
+    for i in df.index:
+        row = df.loc[i]
+        text = row['text'].split(" ")
+        preds = row['pred']
+        truths = row['truths']
+        pred_text = ""
+        truth_text = ""
+        for i, word in enumerate(text):
+            if pred_text == "":
+                pred_text = word + " | " + preds[i]
+            else:
+                pred_text = pred_text + " " + word + " | " + preds[i]
+            if truth_text == "":
+                truth_text = word + " | " + truths[i]
+            else:
+                truth_text = truth_text + " " + word + " | " + truths[i]
+        df.loc[i, "pred_text"] = pred_text
+        df.loc[i, "truth_text"] = truth_text
+    df.to_csv(results_dir + "/"+filename, index=False)
+    return
+
+
+def process_all_results():
+    for filename in os.listdir(results_dir):
+        basic_process_results(filename)
+
+
 
 
 def type_f1(q, pred_list):
